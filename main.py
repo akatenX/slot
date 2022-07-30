f032ba5a6d776c860018556f52da6b49a726d809
akatenX <88500948+akatenX@users.noreply.github.com>
2022-07-30 16:12:37 +0900

    Update main.py
    全部が違う時の処理追加するの忘れてた( ・∇・)

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import random
-naka = ["7", "🕶", "😊", "😭", "🎩"]
-#naka=["7"]
-one = random.choice(naka)
-two = random.choice(naka)
-three=random.choice(naka)
-print(one+two+three)
-if one == two:#1と2がおなじで
-  if two == three:#3も同じなら
-    print("当たり！")#あたり ctx.send
-  else:#同じじゃないなら
-    print("ハズレ！")#はずれ
-else:
-  if two == three:#2と3がおなじなら
-    if three == one:#1もおなじなら
-      print("当たり！")#あたり
-    else:#ちがうなら
-      print("ハズレ")#はずれ
-  elif three == one:#3と1が同じなら
-    if one == two:#2も同じなら
-      print("当たり！")#当たり
-    else:#それ以外
-      print("ハズレ")#はずれ
-      
+@bot.command()
+async def slot(ctx):
+    naka = ["7️⃣", "🕶", "😊", "😭", "🎩", "😶", "🤔", "‼️", "❓"]
+    #naka=["7"]
+    one = random.choice(naka)
+    two = random.choice(naka)
+    three=random.choice(naka)
+    await ctx.send(one+two+three)
+    if one == two:#1と2がおなじで
+      if two == three:#3も同じなら
+        await ctx.send("当たり！")#あたり ctx.send
+      else:#同じじゃないなら
+        await ctx.send("ハズレ")#はずれ
+    else:
+      if two == three:#2と3がおなじなら
+        if three == one:#1もおなじなら
+          await ctx.send("当たり！")#あたり
+        else:#ちがうなら
+          await ctx.send("ハズレ")#はずれ
+      elif three == one:#3と1が同じなら
+        if one == two:#2も同じなら
+          await ctx.send("当たり！")#当たり
+        else:#それ以外
+          await ctx.send("ハズレ")#はずれ
+      else:
+        await ctx.send("ハズレ")
